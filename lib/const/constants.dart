5e8bdd29e3ae4be631d6caf8f96041eb8ad932b0
YAXSHA <nikola.babamov@hotmail.com>
2022-11-02 21:41:40 +0100

    Added constants.dart for title, and workout.dart for workout model

---
+++ lib/const/constants.dart
@@ -0,0 +1,3 @@
+class Constants{
+  static const String titleOfApp = "Floss Fitness App";
+}
---
+++ lib/data/models/workout.dart
@@ -0,0 +1,6 @@
+class Workout{
+  late int workoutId;
+  late DateTime startDateTime;
+  late DateTime endDateTime;
+  late bool isCompleted;
+}
